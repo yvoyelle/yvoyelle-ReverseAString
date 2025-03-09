9e04774efc9d4320731e559cb33a70fda31b8dc2
yvon voyelle <Voyelleyvon1@gmail.com>
2025-03-09 13:54:12 +0000

    Compled tab reverse word

--- src/main/java/StringReverse.java
+++ src/main/java/StringReverse.java
@@ -4,15 +4,23 @@ public class StringReverse {
      * Task: return the reverse of a string.
      * There are multiple good ways to solve this problem.
      *
-     * You could do some math to grab characters from the end of the String using a typical for loop.
-     * For instance, you could use build a String by writing a for loop that decrements rather than increments, grabbing
+     * You could do some math to grab characters from the end of the String using a
+     * typical for loop.
+     * For instance, you could use build a String by writing a for loop that
+     * decrements rather than increments, grabbing
      * characters from the String starting from the end.
      * You could also look into the methods provided by StringBuilder.
      *
      * @param str a String.
      * @return the reverse of str.
-     */
-    public String reverse(String str){
-        return null;
+     */  
+    public String reverse(String str) 
+    {
+        StringBuilder reverse = new StringBuilder();
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse.append(str.charAt(i));
+        }
+        return reverse.toString();
     }
 }
